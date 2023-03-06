083b9f34cf884183d1fdca815563ea1bc00c1903
deogratiasimanuel <119482000+deogratiasimanuel@users.noreply.github.com>
2023-03-06 19:31:05 +0900

    Add files via upload

---
+++ switchharga.php
@@ -0,0 +1,23 @@
+<?php 
+
+$harga = 4;
+switch ($harga) {
+    case 1: 
+        echo "Harga dari barang itu ialah Rp132.000";
+        break;
+    case 2: 
+        echo "Harga dari barang itu ialah Rp86.000";
+        break;
+    case 3: 
+        echo "Harga dari barang itu ialah Rp64.000";
+        break;
+    case 4: 
+        echo "Harga dari barang itu ialah Rp37.000";
+        break;
+    case 5: 
+        echo "Harga dari barang itu ialah Rp28.000";
+        break;
+    default: 
+        echo "Harga Barang tidak diketahui";
+        break;
+}
---
+++ tennarygengan.php
@@ -0,0 +1,4 @@
+<?php 
+
+$bil = 28;
+echo $bil % 2 === 0 ? "$bil merupakan bilangan genap" : "$bil merupakan bilangan ganjil";
